c7776a816142b61177889db8342e8010c37b6b57
Satwik Joel <86243758+nsatwik@users.noreply.github.com>
2025-08-26 23:15:14 +0530

    Delete Scripts/after_install.sh

--- Scripts/after_install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-echo "After install tasks..."
-if [ -f "/opt/tomcat/logs/catalina.out" ]; then
-    sudo rm -f /opt/tomcat/logs/catalina.out
-fi
-echo "After install completed."
